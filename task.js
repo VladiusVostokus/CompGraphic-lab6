8eadf682e5b0b94acb9345297ac8037f04b906f1
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-24 09:49:00 +0200

    Move functions outside main function

--- task.js
+++ task.js
@@ -39,6 +39,63 @@ void main() {
     fragColor = vec4(uAmbientColor + diffuseColor, 1.0);
 }`;
 
+const perspective = (fovy, aspect, near, far) => {
+    var f = Math.tan(Math.PI * 0.5 - 0.5 * fovy);
+    var rangeInv = 1.0 / (near - far);
+
+    return [
+        f / aspect, 0, 0, 0,
+        0, f, 0, 0,
+        0, 0, (near + far) * rangeInv, -1,
+        0, 0, near * far * rangeInv * 2, 0
+    ];
+};
+
+const translate = (translationVec) => {
+    let x = translationVec[0], y = translationVec[1], z = translationVec[2];
+    const matrix = [
+        1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1,
+    ]
+    matrix[12] = matrix[0] * x + matrix[4] * y + matrix[8] * z + matrix[12];
+    matrix[13] = matrix[1] * x + matrix[5] * y + matrix[9] * z + matrix[13];
+    matrix[14] = matrix[2] * x + matrix[6] * y + matrix[10] * z + matrix[14];
+    matrix[15] = matrix[3] * x + matrix[7] * y + matrix[11] * z + matrix[15];
+    return matrix;
+};
+
+const multiply = (a, b) => {
+    var a00 = a[0 * 4 + 0], a01 = a[0 * 4 + 1], a02 = a[0 * 4 + 2], a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0], a11 = a[1 * 4 + 1], a12 = a[1 * 4 + 2], a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0], a21 = a[2 * 4 + 1], a22 = a[2 * 4 + 2], a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0], a31 = a[3 * 4 + 1], a32 = a[3 * 4 + 2], a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0], b01 = b[0 * 4 + 1], b02 = b[0 * 4 + 2], b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0], b11 = b[1 * 4 + 1], b12 = b[1 * 4 + 2], b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0], b21 = b[2 * 4 + 1], b22 = b[2 * 4 + 2], b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0], b31 = b[3 * 4 + 1], b32 = b[3 * 4 + 2], b33 = b[3 * 4 + 3];
+  
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+    ];
+};
+
 function main() {
     const canvas = document.querySelector("#glcanvas");
     const gl = canvas.getContext("webgl2");
@@ -84,7 +141,7 @@ function main() {
 
     const ambientColor = [0, 0.3, 0];
     const lightColor = [1, 1, 1];
-    const lightDirection = [0 ,1, 0];
+    const lightDirection = [1 ,1, 0];
     gl.uniform3f(uAmbientColor, ...ambientColor);
     gl.uniform3f(uLightColor, ...lightColor);
     gl.uniform3f(uLightDirection, ...lightDirection);
@@ -145,63 +202,6 @@ function main() {
 
     const fovY = Math.PI / 4;
     const aspectRatio = canvas.width / canvas.height;
-
-    const perspective = (fovy, aspect, near, far) => {
-        var f = Math.tan(Math.PI * 0.5 - 0.5 * fovy);
-        var rangeInv = 1.0 / (near - far);
-
-        return [
-            f / aspect, 0, 0, 0,
-            0, f, 0, 0,
-            0, 0, (near + far) * rangeInv, -1,
-            0, 0, near * far * rangeInv * 2, 0
-        ];
-    };
-
-    const translate = (translationVec) => {
-        let x = translationVec[0], y = translationVec[1], z = translationVec[2];
-        const matrix = [
-            1,0,0,0,
-            0,1,0,0,
-            0,0,1,0,
-            0,0,0,1,
-        ]
-        matrix[12] = matrix[0] * x + matrix[4] * y + matrix[8] * z + matrix[12];
-        matrix[13] = matrix[1] * x + matrix[5] * y + matrix[9] * z + matrix[13];
-        matrix[14] = matrix[2] * x + matrix[6] * y + matrix[10] * z + matrix[14];
-        matrix[15] = matrix[3] * x + matrix[7] * y + matrix[11] * z + matrix[15];
-        return matrix;
-    }
-
-    const multiply = (a, b) => {
-        var a00 = a[0 * 4 + 0], a01 = a[0 * 4 + 1], a02 = a[0 * 4 + 2], a03 = a[0 * 4 + 3];
-        var a10 = a[1 * 4 + 0], a11 = a[1 * 4 + 1], a12 = a[1 * 4 + 2], a13 = a[1 * 4 + 3];
-        var a20 = a[2 * 4 + 0], a21 = a[2 * 4 + 1], a22 = a[2 * 4 + 2], a23 = a[2 * 4 + 3];
-        var a30 = a[3 * 4 + 0], a31 = a[3 * 4 + 1], a32 = a[3 * 4 + 2], a33 = a[3 * 4 + 3];
-        var b00 = b[0 * 4 + 0], b01 = b[0 * 4 + 1], b02 = b[0 * 4 + 2], b03 = b[0 * 4 + 3];
-        var b10 = b[1 * 4 + 0], b11 = b[1 * 4 + 1], b12 = b[1 * 4 + 2], b13 = b[1 * 4 + 3];
-        var b20 = b[2 * 4 + 0], b21 = b[2 * 4 + 1], b22 = b[2 * 4 + 2], b23 = b[2 * 4 + 3];
-        var b30 = b[3 * 4 + 0], b31 = b[3 * 4 + 1], b32 = b[3 * 4 + 2], b33 = b[3 * 4 + 3];
-      
-        return [
-          b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
-          b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
-          b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
-          b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
-          b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
-          b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
-          b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
-          b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
-          b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
-          b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
-          b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
-          b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
-          b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
-          b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
-          b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
-          b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
-        ];
-    };
 
     let angle = 40.0;
 
